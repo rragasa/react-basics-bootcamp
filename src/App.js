4f3b06c743c835f32afeb1c558e9ef382684fcf6
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-01-08 14:41:42 +0000

    [Task 1] - task completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="container secret-santa-app">
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light">
+      <span className="navbar-brand mb-0 h1">Secret Santa App</span>
+    </nav>
+  )
+}
+
+export default Navbar;
